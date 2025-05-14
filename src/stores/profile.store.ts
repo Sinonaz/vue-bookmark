df431599f11635f36b6a32c4381bb0723da050ce
Isaac <aziksimpson@gmail.com>
2025-05-14 22:09:40 +0200

    Добавляет axios

--- src/stores/profile.store.ts
+++ src/stores/profile.store.ts
@@ -1,14 +1,14 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { ProfileInterface } from '@/interfaces/profile.interface.ts'
-import { API_ROUTES } from '@/api.ts'
+import { API_ROUTES, http } from '@/api.ts'
 
 export const useProfileStore = defineStore('profile', () => {
   const profile = ref<ProfileInterface>()
 
   async function fetchProfile() {
-    const data = await fetch(API_ROUTES.profile)
-    const res = await data.json()
+    const { data } = await http.get<ProfileInterface>(API_ROUTES.profile)
+    const res = data
 
     profile.value = res
   }
