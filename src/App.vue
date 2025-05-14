7387b00bc91acceb641e015c33ade3706eb0598f
Isaac <aziksimpson@gmail.com>
2025-05-14 19:10:04 +0200

    Подключение компонента ProfileAvatar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <script setup lang="ts">
+import ProfileAvatar from '@/components/ProfileAvatar.vue'
+import { API_ROUTES } from '@/api.ts'
+import type { Profile } from '@/interfaces/profile.ts'
+import { onMounted, ref } from 'vue'
+
+const profile = ref<Profile>()
+
+async function fetchProfile() {
+  const data = await fetch(API_ROUTES.profile)
+  const res = await data.json()
+
+  profile.value = res
+}
+
+onMounted(() => {
+  fetchProfile()
+})
 </script>
 
 <template>
   <div class="app">
-    <nav class="nav">Menu</nav>
+    <nav class="nav">
+      <ProfileAvatar v-if="profile" :name="profile.name" />
+    </nav>
     <main class="main">Content</main>
   </div>
 </template>
 
 <style scoped>
+.app {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 100px;
+  padding: 140px 120px;
+}
 
-  .app {
-    display: grid;
-    grid-template-columns: auto 1fr;
-    gap: 100px;
-    padding: 140px 120px;
-  }
-
-  .main {
-    justify-self: end;
-    width: 100%;
-    max-width: 1024px;
-  }
-
+.main {
+  justify-self: end;
+  width: 100%;
+  max-width: 1024px;
+}
 </style>
---
+++ src/components/ProfileAvatar.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const { name } = defineProps<{ name: string }>()
+</script>
+
+<template>
+  <div class="avatar">
+    <img
+      class="avatar__img"
+      src="https://placehold.co/80"
+      width="80"
+      height="80"
+      alt="Аватар пользователя"
+    />
+
+    <p class="avatar__text">
+      Привет, <b>{{ name }}!</b>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.avatar {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+.avatar__img {
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+}
+
+.avatar__text {
+  margin: 0;
+
+  & b {
+    font-weight: 700;
+  }
+}
+</style>
