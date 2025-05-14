0ab72c7b38c4ec26082a1917e84cd1aa9ad1b8bb
Isaac <aziksimpson@gmail.com>
2025-05-14 18:33:02 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="app">
+    <nav class="nav">Menu</nav>
+    <main class="main">Content</main>
+  </div>
+</template>
+
+<style scoped>
+
+  .app {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 100px;
+    padding: 140px 120px;
+  }
+
+  .main {
+    justify-self: end;
+    width: 100%;
+    max-width: 1024px;
+  }
+
+</style>
